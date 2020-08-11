4cd623f35085471cb535ac7d529a46f80278f9f4
saqfish <saqfish@gmail.com>
2020-08-11 06:09:38 -0500

    refactor

--- wird.c
+++ wird.c
@@ -1,42 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct maqrah {
-	int number;
-};
-
-typedef struct maqrah Maqrah;
-
-typedef struct {
-	int number;
-	Maqrah **maqrahs;
-} Juz;
+#include "wird.h"
 
 int
 main(){
-	Juz **juzes;
-	juzes = malloc(sizeof(Juz*) *30);		
-
-	for(int i=0;i<30;i++){
-		juzes[i] = malloc(sizeof(Juz*));		
+	Juz *juzes[SIZE_JUZ];
+	for(int i=0;i<SIZE_JUZ;i++){
+		juzes[i] = malloc(sizeof(*juzes[i]));		
 		juzes[i]->number = i;
-		juzes[i]->maqrahs = malloc(sizeof(Maqrah*) *8);		
-		for(int j=0;j<8;j++){
-			juzes[i]->maqrahs[j] = malloc(sizeof(Maqrah*));		
+		for(int j=0;j<SIZE_MAQRAH;j++){
+			juzes[i]->maqrahs[j] = malloc(SIZE_MAQRAH * sizeof(Maqrah*));		
 			juzes[i]->maqrahs[j]->number = j;
 		}
 	}
 
-	int count;
 
-
-	for(int i=0;i<30;i++){
-		for(int j=0;j<8;j++){
-			count++;
+	for(int i=0;i<SIZE_JUZ;i++){
+		for(int j=0;j<SIZE_MAQRAH;j++){
 			free(juzes[i]->maqrahs[j]);
 		}
 		free(juzes[i]);
 	}
 
-	printf("Count: %d\n", count);
+	printf("Count: %d\n", SIZE_MAQRAH * SIZE_JUZ);
+
+	return 0;
 }
---
+++ wird.h
@@ -0,0 +1,13 @@
+#define SIZE_JUZ 30
+#define SIZE_MAQRAH 8
+
+typedef struct {
+	int number;
+} Maqrah;
+
+typedef struct {
+	int *pages;
+	Maqrah *maqrahs[SIZE_MAQRAH];
+	int number;
+} Juz;
+
